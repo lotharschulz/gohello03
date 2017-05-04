80f82aaf1dbad093bf126447b63f38ad1eac03e4
Lothar Schulz <lothar.sch@gmx.de>
2017-05-04 23:14:24 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+simple golang hello world server project using [hellogopher](https://github.com/cloudflare/hellogopher) make:
+```
+$ make
+$ ./bin/gohello02
+$ Server at http://localhost:1234.
+....
+```
---
+++ hello.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"sync"
+	"fmt"
+)
+
+const (
+  port = ":1234"
+)
+
+var calls = 0
+var mutex = &sync.Mutex{}
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	mutex.Lock()
+	calls++
+	mutex.Unlock()
+	log.Printf("request from %v\ncalls: %d\n", r.RemoteAddr, calls)
+	w.Write([]byte("howdy\n"))
+}
+
+func books(w http.ResponseWriter, r *http.Request) {
+	mutex.Lock()
+	calls++
+	mutex.Unlock()
+	log.Printf("request from %v\ncalls: %d\n", r.RemoteAddr, calls)
+
+	magazinPages := map[string]int{
+		"vogue": 130,
+		"natgeo": 78,
+		"newyorker": 132,
+	}
+
+	magazine := r.URL.Path[len("/magazine/"):]
+	pages := magazinPages[magazine]
+
+	fmt.Fprintf(w, "%s has %d pages.\n", magazine, pages)
+}
+
+func main() {
+	log.Printf("Server at http://localhost%v.\n", port)
+	http.HandleFunc("/magazine/", books)
+	http.HandleFunc("/", helloWorld)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
